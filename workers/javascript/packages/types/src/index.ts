dd30b8d7015d66e76b8fe68d044c887ffe1e3bf7
Superblocks Admin <admin@superblockshq.com>
2024-08-30 16:57:19 +0000

    syncing up to e644dc9d6821899ce454ef188841403673c620c4
    Co-authored-by: Joey Greco <57115019+joeyagreco@users.noreply.github.com>

--- workers/javascript/packages/types/src/index.ts
+++ workers/javascript/packages/types/src/index.ts
@@ -1,5 +1,8 @@
-
+export * from './agent/v1';
 export * from './api/v1';
 export * from './common/v1';
+export * from './kafka/v1';
 export * from './plugins';
 export * from './secrets/v1';
+export * from './security/v1';
+export * from './utils/v1';
