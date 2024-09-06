1b6cffe6ac1505bada5881fb271cd383807f05a7
Superblocks Admin <admin@superblockshq.com>
2024-09-06 15:35:25 +0000

    syncing up to a25494a7ba000c03035bb5cdc66d3b69ca05a6d8
    Co-authored-by: Dan LaMotte <dlamotte@superblockshq.com>

--- pkg/cli/test_helpers.go
+++ pkg/cli/test_helpers.go
@@ -1,27 +1,87 @@
 package cli
 
 import (
+	"reflect"
+	"sync"
+	"sync/atomic"
+
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 )
 
-// TPflagParse creates a local FlagSet mirroring pflag.CommandLine (global FlagSet), calls
-// fs.Parse and binds to viper global.
+var _parseMu sync.Mutex
+var _parseMuCount atomic.Int64
+var _parseMap sync.Map
+
+type TestingT interface {
+	Cleanup(func())
+	Errorf(format string, args ...interface{})
+	FailNow()
+}
+
+type pflagStringSlicer interface {
+	GetSlice() []string
+}
+
+// TPflagParse creates a local FlagSet copy mirroring pflag.CommandLine (global FlagSet), calls
+// fs.Parse and binds to viper global. This function takes a global lock ensuring serialization
+// of all callers. It may be called sequentially within the same `t` (address of `t` is utilized
+// in internal locking) without issue, but calls between several tests will compete for a single
+// lock ensuring serialization of execution. This is primarily due to global `viper` instance.
 //
 // Several packages use pflag.CommandLine global in init() functions which makes testing
-// complicated because Parse may only be called once per FlagSet.
+// complicated because naive Parses using the global FlagSet are not isolated between tests and
+// the "set value" leaks between tests. This function addresses these issues.
 //
-// Another side effect of this pattern is that tests utilizing these global pflags must not call
-// t.Parallel() prior to initialization (reading viper bound values) as it would cause races between
-// tests as to the value.
-func TPflagParse(t require.TestingT, args []string) {
-	// rebind global pflag.CommandLine to local var so we can call Parse many times and bind that
-	// to viper; unfortunately, pflag/viper are managed as runtime globals so tests cannot be
-	// parallel or flakes ensue as a test cannot be guaranteed what values it'll get (globals could
-	// be changed by another test running in parallel)
+// Run tests with `-count=2` to verify proper behavior.
+//
+// Types with special handling:
+//
+//	stringSlice - creates new flag of same type throwing away original pointer reader feature
+func TPflagParse(t TestingT, args []string) {
+	_, ok := _parseMap.Load(t) // only lock once per test
+	if !ok {
+		_parseMu.Lock() // only one test may modify global state at a time
+		_parseMap.Store(t, t)
+	}
+	_parseMuCount.Add(1)
+	t.Cleanup(func() {
+		c := _parseMuCount.Add(-1)
+		if c == 0 {
+			_parseMap.Delete(t)
+			_parseMu.Unlock()
+		}
+	})
+
 	fs := pflag.NewFlagSet("test", pflag.ContinueOnError)
-	fs.AddFlagSet(pflag.CommandLine)
+
+	// fs.AddFlagSet but make copies instead of mutating same global flag
+	pflag.CommandLine.VisitAll(func(flag *pflag.Flag) {
+		if flag.Value.Type() == "stringSlice" {
+			os := flag.Value.(pflagStringSlicer).GetSlice()
+			ns := make([]string, len(os))
+			for i, s := range os {
+				ns[i] = s
+			}
+
+			fs.StringSliceP(
+				flag.Name,
+				flag.Shorthand,
+				ns,
+				flag.Usage,
+			)
+		} else {
+			var newFlag pflag.Flag
+			newFlag = *flag
+			newFlag.Value = reflect.New(reflect.ValueOf(newFlag.Value).Elem().Type()).Interface().(pflag.Value)
+			reflect.ValueOf(newFlag.Value).Elem().Set(reflect.ValueOf(flag.Value).Elem())
+			if fs.Lookup(newFlag.Name) == nil {
+				fs.AddFlag(&newFlag)
+			}
+		}
+	})
+
 	err := fs.Parse(args)
 	require.NoError(t, err)
 	err = viper.BindPFlags(fs)
--- pkg/cli/test_helpers_test.go
+++ pkg/cli/test_helpers_test.go
@@ -12,16 +12,53 @@ const testOptionDefault = "default"
 
 func init() {
 	pflag.String("cli.test.option", testOptionDefault, "test option only")
+	pflag.StringSlice("cli.test.stringslice", []string{"default"}, "test option only")
 }
 
 func TestTPflagParseOk(t *testing.T) {
+	t.Parallel() // just ensures no issues to serialization of TPflagParse
+
 	TPflagParse(t, nil)
 	require.Equal(t, testOptionDefault, viper.GetString("cli.test.option"))
 }
 
 func TestTPflagParseOkNonDefault(t *testing.T) {
+	t.Parallel() // just ensures no issues to serialization of TPflagParse
+
 	TPflagParse(t, []string{
 		"--cli.test.option=nondefault",
 	})
 	require.Equal(t, "nondefault", viper.GetString("cli.test.option"))
+}
+
+func TestTPflagParseOkMultipleTimes(t *testing.T) {
+	t.Parallel() // just ensures no issues to serialization of TPflagParse
+
+	TPflagParse(t, []string{
+		"--cli.test.option=one",
+	})
+	require.Equal(t, "one", viper.GetString("cli.test.option"))
+
+	TPflagParse(t, []string{
+		"--cli.test.option=two",
+	})
+	require.Equal(t, "two", viper.GetString("cli.test.option"))
+}
+
+func TestTPflagParseOkStringSlice(t *testing.T) {
+	t.Parallel() // just ensures no issues to serialization of TPflagParse
+
+	TPflagParse(t, []string{})
+	require.Equal(t, []string{"default"}, viper.GetStringSlice("cli.test.stringslice"))
+
+	TPflagParse(t, []string{
+		"--cli.test.stringslice=one",
+	})
+	require.Equal(t, []string{"one"}, viper.GetStringSlice("cli.test.stringslice"))
+
+	TPflagParse(t, []string{
+		"--cli.test.stringslice=one",
+		"--cli.test.stringslice=two",
+	})
+	require.Equal(t, []string{"one", "two"}, viper.GetStringSlice("cli.test.stringslice"))
 }
